d853abbce08d2f0d7e7d62d4a43c41c61b28fd62
Nickolay Kulikov <fipsiksi@yandex.ru>
2019-06-07 22:12:24 +0300

    add bootstrap
    add all things from bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import * as React from "react";
 import { render } from "react-dom";
 
